43d94af857f43b07bcc9b668dbd7417dc8332c13
Antoine07 <antoine.lucsko@gmail.com>
2023-03-10 10:34:28 +0100

    database

--- TP/Examen/myApp/app/database.js
+++ TP/Examen/myApp/app/database.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb'
 
 // Déclaration de la connectionString
 const CONNECTION_STRING = "mongodb://root:example@mongo:27017"; // Avec Docker
-// const CONNECTION_STRING = 'mongodb://localhost:27017'; // Installation locale de MongoDB
+// const CONNECTION_STRING = 'mongodb://127.0.0.1:27017'; // Installation locale de MongoDB
 
 const client = new MongoClient(CONNECTION_STRING);
 let db = null;
@@ -18,7 +18,6 @@ export function openDatabase(dbName) {
   });
 };
 
-// Fonction permettant d'obtenir un object 'collection' Mongo
 export function getCollection(collectionName) {
   console.log(`Retrieving collection ${collectionName} …`);
 
